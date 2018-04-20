d0dc4b9b5cf0106e91c8e15fdeb6389439a2777b
sgg2123 <sgg2123@columbia.edu>
2018-04-20 02:47:14 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,8 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     puts phrase
-    
+    counter += 1
+    if counter == number_of_times
   
 end
 
